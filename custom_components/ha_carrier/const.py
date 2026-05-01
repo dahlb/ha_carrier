678d49c606438da0f6c2792df53a309f66c6fb8a
GitHub Action <action@github.com>
2026-05-01 09:56:00 +0000

    chore: bumping version to 2.19.5 [skip ci]

--- custom_components/ha_carrier/const.py
+++ custom_components/ha_carrier/const.py
@@ -2,7 +2,7 @@
 
 from homeassistant.const import CONF_PASSWORD, CONF_UNIQUE_ID, CONF_USERNAME, Platform
 
-VERSION: str = "2.19.4"
+VERSION: str = "2.19.5"
 
 # Configuration Constants
 DOMAIN: str = "ha_carrier"
